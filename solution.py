2c3a82000610c22e9a6c512899181261ada4bbac
Kimmirikwa <kimrodrikwa@gmail.com>
2019-08-04 21:42:57 +0300

    add Month column and remove Date

--- solution.py
+++ solution.py
@@ -11,3 +11,17 @@ print(input_data.head())
 
 # check for missing values
 print(input_data.isnull().values.any())  # False, this dataframe does not have any missing values
+
+# we need to extract a Month from a given data as we do not have month data given
+# will use this function below
+def get_month(row):
+	return row['Date'][4:]
+
+# add the month column
+input_data['Month'] =  input_data.apply(lambda row: get_month(row), axis=1)
+
+# we no longer need 'Date' column in our data
+input_data.drop('Date', axis=1, inplace=True)
+
+# the columns of the dataframe after adding 'Month' and dropping 'Date'
+print(input_data.columns) # MSISDN, Network, Product, Amount, Month
