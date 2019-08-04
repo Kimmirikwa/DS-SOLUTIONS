1dfaa8bea4eb84bdace18821b8ab70e25c4c20c8
Kimmirikwa <kimrodrikwa@gmail.com>
2019-08-04 21:30:05 +0300

    a little data exploration

--- solution.py
+++ solution.py
@@ -2,3 +2,12 @@ import pandas as pd
 
 # dataframe of the input data
 input_data = pd.read_csv('data/input.csv')
+
+# the columns of the dataframe
+print(input_data.columns) # MSISDN, Network, Date, Product, Amount
+
+# first 5 rows of the data
+print(input_data.head())
+
+# check for missing values
+print(input_data.isnull().values.any())  # False, this dataframe does not have any missing values
